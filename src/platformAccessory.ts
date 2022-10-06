c48755871ccd985f8efe01ede9b2df09ce23bbaf
Vincenzo Chianese <vincenz.chianese@icloud.com>
2022-10-06 11:45:15 -0600

    add oven temperature

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,5 +1,4 @@
 import type { PlatformAccessory } from 'homebridge';
-import { compact } from 'lodash';
 import { ERD_TYPES } from './constants';
 import { SmartHQPlatform } from './platform';
 import type { SmartHqContext } from './platform';
@@ -17,9 +16,8 @@ export class SmartHQOven {
 			.setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.serial)
 			.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.nickname);
 
-		compact(
-			this.accessory.context.device.features.map(feature => {
-				/* [
+		this.accessory.context.device.features.map(feature => {
+			/* [
 					'COOKING_V1_ACCENT_LIGHTING',
 					'COOKING_V1_EXTENDED_COOKTOP_FOUNDATION',
 					'COOKING_V1_MENU_TREE',
@@ -28,48 +26,71 @@ export class SmartHQOven {
 					'COOKING_V2_UPPER_CAVITY_REMOTE_PRECISION_COOK',
 				]; */
 
-				switch (feature) {
-					case 'COOKING_V1_UPPER_OVEN_FOUNDATION': {
-						const ovenLight =
-							this.accessory.getService('Upper Oven Light') ||
-							this.accessory.addService(this.platform.Service.Lightbulb, 'Upper Oven Light', 'Oven');
+			switch (feature) {
+				case 'COOKING_V1_UPPER_OVEN_FOUNDATION': {
+					const ovenLight =
+						this.accessory.getService('Upper Oven Light') ||
+						this.accessory.addService(this.platform.Service.Lightbulb, 'Upper Oven Light', 'Oven');
 
-						ovenLight
-							.getCharacteristic(this.platform.Characteristic.On)
-							.onGet(() =>
-								axios
-									.get(`/appliance/${accessory.context.device.applianceId}/erd/${ERD_TYPES.UPPER_OVEN_LIGHT}`)
-									.then(r => parseInt(r.data.value) !== 0),
-							)
-							.onSet((value: boolean) =>
-								axios
-									.post(
-										`/appliance/${accessory.context.device.applianceId}/erd/${ERD_TYPES.UPPER_OVEN_LIGHT}`,
-										this.erdPayload(ERD_TYPES.UPPER_OVEN_LIGHT, value),
-									)
-									.then(() => undefined),
-							);
+					ovenLight
+						.getCharacteristic(this.platform.Characteristic.On)
+						.onGet(() => this.readErd(ERD_TYPES.UPPER_OVEN_LIGHT).then(r => parseInt(r) !== 0))
+						.onSet((value: boolean) => this.writeErd(ERD_TYPES.UPPER_OVEN_LIGHT, value));
 
-						return ovenLight;
-					}
+					const ovenThermostat =
+						this.accessory.getService('Upper Oven Thermostat') ||
+						this.accessory
+							.addService(this.platform.Service.Thermostat, 'Upper Oven Thermostat', 'Oven')
+							.getCharacteristic(this.platform.Characteristic.TargetTemperature)
+							.onGet(async () => {
+								const erdVal = await this.readErd(ERD_TYPES.UPPER_OVEN_COOK_MODE);
 
-					case 'COOKING_V1_ACCENT_LIGHTING': {
-						const service =
-							this.accessory.getService('Accent Light') ||
-							this.accessory.addService(this.platform.Service.Lightbulb, 'Accent Light', 'Stove');
-					}
+								const b = Buffer.from(erdVal, 'hex');
+								return fToC(b.readUint16BE(1));
+							})
+							.onSet(async (value: number) => {
+								const fTarget = cToF(value);
+
+								const erdVal = await this.readErd(ERD_TYPES.UPPER_OVEN_COOK_MODE);
+								const b = Buffer.from(erdVal, 'hex');
+								b.writeUint16BE(fTarget, 1);
+
+								return this.writeErd(ERD_TYPES.UPPER_OVEN_COOK_MODE, b.toString('hex'));
+							});
 				}
-			}),
-		);
+
+				case 'COOKING_V1_ACCENT_LIGHTING': {
+					const service =
+						this.accessory.getService('Accent Light') ||
+						this.accessory.addService(this.platform.Service.Lightbulb, 'Accent Light', 'Stove');
+				}
+			}
+		});
 	}
 
-	erdPayload(erd: string, value: string | boolean) {
-		return {
-			kind: 'appliance#erdListEntry',
-			userId: this.accessory.context.userId,
-			applianceId: this.accessory.context.device.applianceId,
-			erd,
-			value: typeof value === 'boolean' ? (value ? '01' : '00') : value,
-		};
+	readErd(erd: string): Promise<string> {
+		return axios
+			.get(`/appliance/${this.accessory.context.device.applianceId}/erd/${erd}`)
+			.then(d => String(d.data.value));
 	}
+
+	writeErd(erd: string, value: string | boolean) {
+		return axios
+			.post(`/appliance/${this.accessory.context.device.applianceId}/erd/${erd}`, {
+				kind: 'appliance#erdListEntry',
+				userId: this.accessory.context.userId,
+				applianceId: this.accessory.context.device.applianceId,
+				erd,
+				value: typeof value === 'boolean' ? (value ? '01' : '00') : value,
+			})
+			.then(() => undefined);
+	}
+}
+
+function cToF(celsius) {
+	return (celsius * 9) / 5 + 32;
+}
+
+function fToC(fahrenheit) {
+	return ((fahrenheit - 32) * 5) / 9;
 }
